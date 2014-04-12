82a508983da833afeecfa73681ad437ede0d1156
Cyprien Le Pannérer <cyplp@free.fr>
2014-04-12 19:00:11 +0200

    Version primitive du old

--- paulla.ircbot/src/paulla/ircbot/plugins/Urls.py
+++ paulla.ircbot/src/paulla/ircbot/plugins/Urls.py
@@ -1,8 +1,12 @@
+import sqlite3
+from os.path import exists, dirname, expanduser
+import re
+from urllib.parse import urlparse
+from os import makedirs
+
 import irc3
 import requests
-import re
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 
 
 @irc3.plugin
@@ -14,17 +18,76 @@ class Urls:
         self.bot = bot
         self.log = self.bot.log
 
+        if 'paulla.ircbot.plugins.Urls' in self.bot.config\
+                and 'db' in self.bot.config['paulla.ircbot.plugins.Urls']\
+                and self.bot.config['paulla.ircbot.plugins.Urls']['db']:
+            db = self.bot.config['paulla.ircbot.plugins.Urls']['db']
+        else:
+            db = '~/.irc3/Urls.db'
+        if '~' in db:
+            db = expanduser(db)
+        if not exists(db):
+            if not exists(dirname(db)):
+                makedirs(dirname(db))
+            open(db, 'a').close()
+        self.conn = sqlite3.connect(db)
+        cur = self.conn.cursor()
+        cur.execute('''create table if not exists url
+                (id_url integer primary key,
+                value text,
+                title text,
+                dt_inserted datetime,
+                nick text
+                )''')
+        cur.execute('''create table if not exists tag
+                (id_tag integer primary key,
+                id_url interger,
+                value text
+                dt_inserted datetime,
+                nick text
+                )''')
+
+        cur.execute('''create table if not exists old
+                (id_old integer primary key,
+                value text
+                dt_inserted datetime,
+                nick text
+                )''')
+        self.conn.commit()
+        cur.close()
+
+
     @irc3.event(irc3.rfc.PRIVMSG)
     def url(self, mask, event, target, data):
         """
         parse and reply url title
         """
-        bot = self.bot
+
+        print("plop")
+        print(mask, event, target, data)
         urls = re.findall('(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)', data)
+
+        nick = mask.split('!')[0]
+
         for url in urls:
-            r = requests.get(url)
-            soup = BeautifulSoup(r.content)
+            req = requests.get(url)
+            soup = BeautifulSoup(req.content)
             title = soup.title.string.encode('ascii', 'ignore').decode('ascii', 'ignore')
             domain = urlparse(url).netloc.split(':')[0]
             self.bot.privmsg(target, "Title: %s - (at %s)" % (title, domain))
+
+            cur = self.conn.cursor()
+
+            cur.execute("SELECT dt_inserted, nick from url where value='%s'" % url)
+            data = cur.fetchall()
+
+            if data:
+                self.old(target, nick, data[0][0])
+            else:
+                cur.execute("INSERT INTO url(value, title, nick, dt_inserted) VALUES('%s', '%s', '%s', datetime('now')) ;" % (url, title, nick))
+                self.conn.commit()
+            cur.close()
+
+    def old(self, target, nick, dt_inserted):
+        self.bot.privmsg(target, '%s pfff' % nick)
 # end of file
